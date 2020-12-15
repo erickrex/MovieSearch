8b0b5c425c49684d6c505386d25a36e7ac5232d5
erickrex <59777190+erickrex@users.noreply.github.com>
2020-12-15 23:54:54 +0100

    Update movieCard.js

--- movieCard.js
+++ movieCard.js
@@ -4,7 +4,7 @@ export default function MovieCard({movie}){
     return (
          <div className="card"  >
             <img className="card--image"
-                src={`https://image.tmdb.org/t/p/w185_and_h278_bestv2/${movie.poster_path}`}
+                src={`https://image.tmdb.org/....${movie.poster_path}`}
                 alt={movie.title + ' poster'}
                 />
             <div className="card--content">
